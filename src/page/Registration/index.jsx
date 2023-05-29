2c67461f95a7d84a8a45db993606686f0ecd572f
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2023-05-29 23:16:23 +0600

    login page formik funcitonality add

--- src/page/Registration/index.jsx
+++ src/page/Registration/index.jsx
@@ -3,8 +3,22 @@ import "./style.css";
 // react icon
 import { BsLinkedin } from "react-icons/bs";
 import { Button, Input } from "@chakra-ui/react";
+import { useFormik } from "formik";
 
 const Registration = () => {
+  const initialValues = {
+    fullname: "",
+    email: "",
+    password: "",
+    confirmpassword: "",
+  };
+  const formik = useFormik({
+    initialValues: initialValues,
+    onSubmit: () => {
+      console.log("hello");
+    },
+  });
+  console.log(formik.values);
   return (
     <>
       <div className="registration">
@@ -17,35 +31,44 @@ const Registration = () => {
             <h4>Free register and you can enjoy it</h4>
           </div>
           <div className="registration-form">
-            <form>
+            <form onSubmit={formik.handleSubmit}>
               <Input
+                onChange={formik.handleChange}
                 type="text"
                 name="fullname"
+                value={formik.values.fullname}
                 placeholder="Full Name"
                 size="lg"
               />
               <Input
+                onChange={formik.handleChange}
                 mt="5"
-                type="text"
+                type="email"
                 name="email"
+                value={formik.values.email}
                 placeholder="Email Address"
                 size="lg"
               />
               <Input
+                onChange={formik.handleChange}
                 mt="5"
                 type="password"
                 name="password"
+                values={formik.values.password}
                 placeholder="Password"
                 size="lg"
               />
               <Input
+                onChange={formik.handleChange}
                 mt="5"
                 type="passord"
                 name="confirmpassword"
+                value={formik.values.confirmpassword}
                 placeholder="Confirm Password"
                 size="lg"
               />
               <Button
+                type="submit"
                 className="reg-btn"
                 mt="6"
                 width="full"
