51dded46b7a8fbf63bd44b996dcb0a3939fd7dd6
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2023-06-03 10:41:22 +0600

    react router dom inistall and implement

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Registration from "./page/Registration";
+import { Login } from "./page/Login";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/registration" element={<Registration />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Route>
+    )
+  );
   return (
     <>
-      <Registration />
+      <RouterProvider router={router}></RouterProvider>
     </>
   );
 }
