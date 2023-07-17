1addefe699cff2ce11f07f65189ea5b10320a26d
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2023-07-17 18:11:20 +0600

    navbar design done

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -4,8 +4,15 @@ import "./style.css";
 import { Container } from "@chakra-ui/react";
 import { Input } from "@chakra-ui/react";
 import { BiSearchAlt2 } from "react-icons/bi";
+import { FaNetworkWired } from "react-icons/fa";
+import { BsBag } from "react-icons/bs";
+import { AiFillNotification } from "react-icons/ai";
+import { FaSms } from "react-icons/fa";
+import { AiTwotoneHome } from "react-icons/Ai";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <div className="navbar-main">
       <Container maxW="6xl">
@@ -21,7 +28,45 @@ const Navbar = () => {
               <BiSearchAlt2 />
             </div>
           </div>
-          <div className="nav-menu"></div>
+          <div className="nav-menu">
+            <ul>
+              <li onClick={() => navigate("/")}>
+                <div className="nav-icon">
+                  <AiTwotoneHome />
+                </div>
+                <div className="nav-list">Home</div>
+              </li>
+              <li onClick={() => navigate("/about")}>
+                <div className="nav-icon">
+                  <FaNetworkWired />
+                </div>
+                <div className="nav-list">My Network</div>
+              </li>
+
+              <li>
+                <div className="nav-icon">
+                  <BsBag />
+                </div>
+                <div className="nav-list">Jobs</div>
+              </li>
+              <li>
+                <div className="nav-icon">
+                  <FaSms />
+                </div>
+                <div className="nav-list"> Messaging</div>
+              </li>
+              <li>
+                <div className="nav-icon">
+                  <AiFillNotification />
+                </div>
+                <div className="nav-list">Notification</div>
+              </li>
+              <li>
+                <div className="usericon"></div>
+                <div className="nav-list">Me</div>
+              </li>
+            </ul>
+          </div>
         </div>
       </Container>
     </div>
--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./style.css";
+import { Container } from "@chakra-ui/react";
 
 export const Home = () => {
-  return <div className="home-box">This is home page</div>;
+  return (
+    <div className="home-box">
+      <Container maxW="6xl">
+        <h1 style={{ color: "red" }}>This is home page</h1>
+      </Container>
+    </div>
+  );
 };
