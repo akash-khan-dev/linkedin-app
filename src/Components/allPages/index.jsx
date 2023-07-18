13b32d32a796f30fe3b814dd105a1aa58caeb723
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2023-07-18 21:00:52 +0600

    pages component design

--- src/Components/allPages/index.jsx
+++ src/Components/allPages/index.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 import "./style.css";
+import pageProfile from "../../assets/images/profile.jpg";
+import { Avatar, Box, Button, Flex, Text } from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
 
 const Allpages = () => {
-  return <div>Allpages</div>;
+  return (
+    <>
+      <div className="pages-container">
+        <div className="pages-heading">
+          <h2>Add to your feed</h2>
+        </div>
+        <div className="single-page">
+          <div className="page-content">
+            <Flex>
+              <Avatar src={pageProfile} />
+              <Box ml="3">
+                <Text fontWeight="bold">Segun Adebayo</Text>
+                <Text fontSize="sm">UI Engineer</Text>
+                <Button
+                  rightIcon={<ArrowForwardIcon />}
+                  colorScheme="teal"
+                  variant="outline"
+                >
+                  Call us
+                </Button>
+              </Box>
+            </Flex>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Allpages;
