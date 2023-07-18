2827144962b8cf360251e06fb1249bb7a7acb23f
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2023-07-18 19:38:37 +0600

    create post design something done

--- src/Components/creactePost/index.jsx
+++ src/Components/creactePost/index.jsx
@@ -1,8 +1,77 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./style.css";
+import profile from "../../assets/images/profile.jpg";
+import { Button, Input, useDisclosure } from "@chakra-ui/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
 
 const CreactePost = () => {
-  return <div>CreactePost</div>;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const handleOpenModal = () => {
+    onOpen();
+  };
+  const chooseFile = useRef();
+  return (
+    <>
+      <div className="container">
+        <div className="create-post-wrapper">
+          <div className="user-profile">
+            <div className="user-img">
+              <img src={profile} alt="profile" />
+            </div>
+          </div>
+          <div onClick={handleOpenModal} className="user-write-box">
+            <Input placeholder="Start post" />
+          </div>
+        </div>
+      </div>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader></ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <div className="modal-header">
+              <div className="user-profile">
+                <div className="user-img">
+                  <img src={profile} alt="profile" />
+                </div>
+              </div>
+              <div className="modal-user-name">
+                <h1>akash khan</h1>
+              </div>
+            </div>
+            <div className="modal-write-box">
+              <textarea
+                rows="4"
+                cols="50"
+                placeholder="What do you want to talk about?"
+              ></textarea>
+            </div>
+          </ModalBody>
+
+          <ModalFooter>
+            <input type="file" hidden ref={chooseFile} />
+            <Button
+              colorScheme="blue"
+              mr={3}
+              onClick={() => chooseFile.current.click()}
+            >
+              Select Images
+            </Button>
+            <Button variant="ghost">Post</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
 
 export default CreactePost;
